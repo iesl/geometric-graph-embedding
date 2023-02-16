8afaeb7b421754c5a96f0de198b82e60a6ba0bd1
brozonoyer <brozonoyer@iesl.cs.umass.edu>
2023-02-16 15:00:19 -0500

    add name to sweep config

--- scripts/hierarchical_negative_sampling_auto_sweep.py
+++ scripts/hierarchical_negative_sampling_auto_sweep.py
@@ -14,13 +14,16 @@ def init_sweeps():
 
     config = yaml.safe_load(Path("./bin/learning/hierarchical_negative_sampling.yaml").read_text())
 
-    ids = []
+    sweep_names = []
     sweep_ids = []
+
+    # TRANSITIVE CLOSURE
     for TC in ["True", "False"]:
 
         data_path = f"data/graphs13/balanced_tree/branching=10-log_num_nodes=13-transitive_closure={TC}/"
         config = set_command_in_config(config=config, command="--data_path", value=data_path)
 
+        # NEGATIVE SAMPLING STRATEGIES
         for negative_sampling in ["random", "hierarchical:exact", "hierarchical:uniform", "hierarchical:descendants"]:
 
             if negative_sampling == "random":
@@ -30,17 +33,18 @@ def init_sweeps():
                 config = set_command_in_config(config=config, command="--negative_sampler", value=negative_sampler)
                 config = set_command_in_config(config=config, command="--hierarchical_negative_sampling_strategy", value=strategy)
 
-            id = f"TC:{TC}_{negative_sampling}"
-            ids.append(id)
+            sweep_name = f"TC:{TC}_{negative_sampling}"
+            config["name"] = sweep_name
+            sweep_names.append(sweep_name)
 
             sweep_id = wandb.sweep(sweep=config, entity="brozonoyer", project="hierarchical-negative-sampling")
             sweep_ids.append(sweep_id)
 
-    return ids, sweep_ids
+    return sweep_names, sweep_ids
 
 
 if __name__ == '__main__':
 
-    ids, sweep_ids = init_sweeps()
-    for i in range(len(ids)):
-        print(f"{ids[i]}\t{sweep_ids[i]}")
+    sweep_names, sweep_ids = init_sweeps()
+    for i in range(len(sweep_names)):
+        print(f"{sweep_names[i]}\t{sweep_ids[i]}")
