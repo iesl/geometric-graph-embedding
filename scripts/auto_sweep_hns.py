4671c49b0628734be658ea6ad80a9346e9df44da
brozonoyer_umass_edu <brozonoyer_umass_edu@login1.unity.rc.umass.edu>
2023-03-15 01:04:16 +0000

    work on auto sweep script

--- scripts/auto_sweep_hns.py
+++ scripts/auto_sweep_hns.py
@@ -1,10 +1,4 @@
-# input: model_type, dim
-# output: a list of "wandb sweep" command
-
-
 import argparse
-import json
-import os
 from pathlib import Path
 
 import wandb
@@ -13,7 +7,7 @@ import wandb
 def config_generation(negative_sampling, path):
     graph_type, graph = path.split("/")[-2:]
     sweep_config = {
-        "name": f"{graph_type}_{graph}_{negative_sampling}",
+        "name": f"{graph_type}:::{graph}:::{negative_sampling}",
         # "controller": {"type": "local"},
         "program": "scripts/box-training-methods",
         "command": [
@@ -33,6 +27,7 @@ def config_generation(negative_sampling, path):
             "--log_eval_batch_size=17",
             "--epochs=10000",
             "--data_path=" + path,
+            "--wandb",
         ],
         "method": "bayes",
         "metric": {"goal": "maximize", "name": "[Train] F1"},
@@ -56,92 +51,23 @@ def config_generation(negative_sampling, path):
     return sweep_config
 
 
-def main(config):
+def main():
 
-    if config.bayes_run == True:
-        if not os.path.exists("sweeps_configs"):
-            os.makedirs("sweeps_configs")
-        fout = open(
-            f"sweeps_configs/{config.negative_sampling}_{config.data_path.replace('/','_')}.jsonl",
-            "w+",
-        )
-    count_sweep = 0
-    count_start_sweep = 0
-    count_halfway_sweep = 0
-    count_ninty_sweep = 0
-    count_finished_sweep = 0
-    count_finished_runs = 0
-    for path in Path(config.data_path).glob("**/*log_num_nodes=*"):
-        count_sweep += 1
-        count_this_sweep = 0
-        best_hyperparams = None
-        best_metric = 0.0
-        target_filename = f"hns_results/{config.negative_sampling}/*metric"
-        for f in path.glob(target_filename):
-            # print(f)
-            count_finished_runs += 1
-            count_this_sweep += 1
-            params, metrics = open(str(f)).read().split("\n")
-            params = json.loads(params)
-            metrics = json.loads(metrics)
-            metric = metrics[0]["F1"]
-            if metric > best_metric:
-                best_hyperparams = params
+    for negative_sampling in ["random", "hierarchical:exact", "hierarchical:uniform", "hierarchical:descendants"]:
+        for graph, partition in [("balanced_tree", "gypsum-titanx"), ("nested_chinese_restaurant_process", "gypsum-1080ti"), ("price", "gypsum-2080ti")]:
+            for path in Path(f"data/graphs13/{graph}").glob("**/*log_num_nodes=*"):
 
-        if count_this_sweep > 0:
-            count_start_sweep += 1
-        if count_this_sweep > config.max_run / 2:
-            count_halfway_sweep += 1
-        if count_this_sweep > config.max_run * 0.95:
-            count_ninty_sweep += 1
-        if count_this_sweep >= config.max_run:
-            count_finished_sweep += 1
+                print(f"Initializing sweep/command for {negative_sampling}, {graph}, {partition}")
 
-        print(f"{count_this_sweep} / {config.max_run} finished under {str(path)}")
+                sweep_config = config_generation(
+                    negative_sampling=negative_sampling, path=str(path)
+                )
+                sweep_id = wandb.sweep(sweep_config, entity="hierarchical-negative-sampling", project="hns")
 
-        # If mode is bayes run, check each sweep directory.
-        # If there is less then 95% results, clean the result and do a new sweep.
-        if config.bayes_run == True and count_this_sweep <= 0.95 * config.max_run:
-            print("deleting saved results in this sweep")
-            target_filename = f"hns_results/{config.negative_sampling}/*metric"
-            for f in path.glob(target_filename):
-                f.unlink()
-
-            sweep_config = config_generation(
-                negative_sampling=config.negative_sampling, path=str(path)
-            )
-            sweep_id = wandb.sweep(sweep_config, entity="hierarchical-negative-sampling", project="hns")
-            os.system(
-                f"sh bin/launch_train_sweep.sh hierarchical-negative-sampling/hns/{sweep_id} {config.partition} {config.max_run} "
-            )
-            fout.write(f"{sweep_id} {json.dumps(sweep_config)}\n")
-
-    if config.bayes_run:
-        fout.close()
-
-    print(f"# sweep started: {count_start_sweep}/{count_sweep}")
-    print(f"# sweep halfway: {count_halfway_sweep}/{count_sweep}")
-    print(f"# sweep 95% finished: {count_ninty_sweep}/{count_sweep}")
-    print(f"# sweep finished: {count_finished_sweep}/{count_sweep}")
-    print(f"# run finished: {count_finished_runs}/{count_sweep * config.max_run}")
+                with open(f"./hns_wandb_commands.sh", "a+") as f:
+                    f.write(f"sh ../bin/launch_train_sweep.sh hierarchical-negative-sampling/hns/{sweep_id} {partition} 100 \n")
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(
-        description="Submit multiple sweeps over slurm servers."
-        + "script will only check current status if running without --best_run or --bayes_run"
-    )
-    parser.add_argument(
-        "--bayes_run",
-        action="store_true",
-        help="do bayes hyper-parameter search for each sweep"
-        + "(CAUTION: this will clear all existing results under each sweep data path (if not completed))",
-    )
-    parser.add_argument("--negative_sampling", type=str,
-                        choices=["random", "hierarchical:exact", "hierarchical:uniform", "hierarchical:descendants"])
-    parser.add_argument("--partition", type=str, default="gypsum-titanx")
-    parser.add_argument("--max_run", type=int, default=100)
-    parser.add_argument("--data_path", type=str, default="data/graphs/")
-    config = parser.parse_args()
-    main(config)
+    main()
